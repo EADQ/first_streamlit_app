7f1fab741a720d78c0c9f9025c86ffe8ea6eb977
EADQ <easoftwareengineer@gmail.com>
2023-07-20 12:50:49 -0600

    working with a csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
 
+# MAKING BASE MENU
 st.title('My Parents New Healthy Diner')
 st.header('Breakfast Menu')
 st.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -7,3 +9,11 @@ st.text('🥗 Kale, Spinach & Rocket Smoothie')
 st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
 st.header('🍌 🥭 Build Your Own Fruit Smoothie 🥝 🍇')
+
+# IMPORTING CSV TABLE
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# CALLING THE DATAFRAME WITH STREAMLIT
+streamlit.dataframe(my_fruit_list)
+
+
