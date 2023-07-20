ebd2bb840d99756ece027644c63f28e3216d4244
EADQ <easoftwareengineer@gmail.com>
2023-07-20 14:29:13 -0600

    adding some interactions

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # LET'S PUT A PICK LIST HER SO THEY CAN PICK THE FRUIT THEY WANT TO INCLUDE
-st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-
+fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # DISPLAY THE TABLE ON THE PAGE
-st.dataframe(my_fruit_list)
+st.dataframe(fruits_to_show)
