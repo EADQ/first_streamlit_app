3a8f0184e860603242fb894506f2aff3ce1bf88c
EADQ <easoftwareengineer@gmail.com>
2023-07-20 14:15:07 -0600

    adding some interactions

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,11 @@ st.header('🍌 🥭 Build Your Own Fruit Smoothie 🥝 🍇')
 
 # IMPORTING CSV TABLE
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # ADDING INTERACTION WITH THE MENU
 # LET'S PUT A PICK LIST HERE SO THE CAN PICK THE FRUIT THEY WANT TO INCLUDE
 st.multiselect("Pick some fruits:", list(my_fruit_list.index))
-my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # DISPLAY THE TABLE ON THE PAGE
 st.dataframe(my_fruit_list)
