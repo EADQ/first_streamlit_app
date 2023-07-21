bf57c39d7f0fac95a67816f7a106e00613c06ffc
EADQ <easoftwareengineer@gmail.com>
2023-07-21 14:18:49 -0600

    adding format to json import

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,7 @@ st.dataframe(fruits_to_show)
 st.header("Fruityvice Fruit Advice!")
 
 # CALLING RHE FRUITYVICE API'
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # WRITE YOUR OWN COMMENT -WHAT DOES THE NEXT LINE DO? 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
