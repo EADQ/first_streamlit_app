d15d9875d8265be593bd1d897d418d7c6cc80166
EADQ <easoftwareengineer@gmail.com>
2023-07-21 23:56:05 -0600

    adding get fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,7 +82,11 @@ def insert_row_snowflake(new_fruit):
         my_cur.execute("insert into fruit_load_list values ('from setreamlite')")
         return "Thanks for adding " + new_fruit
 
-
+add_my_fruit = st.text_input('What fruit would you like to add?')
+if st.button('Add a Fruit to the List')
+    my_cnx = snowflake.connector.connect(**st.secrets['snowflake'])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    st.text(back_from_function)
 
 
 
