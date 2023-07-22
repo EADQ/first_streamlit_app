ae8fcae91ae2157cb639f319fb6e985396e7020a
EADQ <easoftwareengineer@gmail.com>
2023-07-21 22:57:55 -0600

    introducing try exept to fix some troubles

--- 2streamlit_app.py
+++ 2streamlit_app.py
@@ -31,22 +31,17 @@ st.dataframe(fruits_to_show)
 # NEW SECTION TO SHOW ADVICE
 st.header("Fruityvice Fruit Advice!")
 
-# CREATE THE REPEATABLE CODE BLOCK (CALLED A FUNCTION)
-def get_fruityvice_data(this_fruit_choice):
-# CALLING RHE FRUITYVICE API'
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# WRITE YOUR OWN COMMENT -WHAT DOES THE NEXT LINE DO? 
-        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-        return fruityvice_normalized
 # ADDING FRUIT CHOICE
-
 try:
     fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
     if not fruit_choice:
         st.error("Please select a fruit to get information.")
     else:
-        back_from_function = get_fruityvice_data(fruit_choice)
+# CALLING RHE FRUITYVICE API'
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+# WRITE YOUR OWN COMMENT -WHAT DOES THE NEXT LINE DO? 
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # WRITE YOUR OWN COMMENT - WHAT DOES THIS DO?
         st.dataframe(fruityvice_normalized)
 
